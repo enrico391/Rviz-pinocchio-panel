a29a21a6ba2df07a9ac2c323bdc39df0f46e0607
Enrico Moro <enrimoro003@gmail.com>
2025-03-19 10:58:08 +0100

    New rviz panel visualizer for pinocchio actions

---
+++ include/rviz_panel_pinocchio_tiago/demo_panel.hpp
@@ -0,0 +1,133 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2024, Metro Robots
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of Metro Robots nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+
+/* Author: David V. Lu!! */
+
+#ifndef RVIZ_PANEL_PINOCCHIO_TIAGO__DEMO_PANEL_HPP_
+#define RVIZ_PANEL_PINOCCHIO_TIAGO__DEMO_PANEL_HPP_
+
+#include <filesystem>
+#include "pinocchio/algorithm/frames.hpp"
+#include "pinocchio/algorithm/geometry.hpp"
+#include "pinocchio/algorithm/jacobian.hpp"
+#include "pinocchio/algorithm/joint-configuration.hpp"
+#include "pinocchio/parsers/urdf.hpp"
+#include "pinocchio/parsers/srdf.hpp"
+#include "pinocchio/collision/collision.hpp" 
+#include "pinocchio/collision/distance.hpp" 
+#include "ament_index_cpp/get_package_share_directory.hpp"
+#define PINOCCHIO_ENABLE_COMPATIBILITY_WITH_VERSION_2
+
+#include <QLabel>
+#include <QPushButton>
+#include <QComboBox>
+#include <rviz_common/panel.hpp>
+#include <rviz_common/ros_integration/ros_node_abstraction_iface.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <sstream>
+
+
+namespace rviz_panel_pinocchio_tiago
+{
+
+
+class PinocchioManager {
+  public:
+    PinocchioManager() = default;
+
+    PinocchioManager(const std::string& urdf_xml, const std::string& package_share_directory);
+  
+    void setConfiguration();
+    std::vector<std::string>  getConfiguration();
+    void performForwardKinematics();
+    void performJacobian(const std::string& arm_link);
+    std::vector<std::string> performCollisionCheck();
+    std::string getFrameTransform(const std::string& arm_link);
+  
+  private:
+    pinocchio::Model model;
+    pinocchio::Data data;
+    pinocchio::GeometryModel visual_model;
+    pinocchio::GeometryModel collision_model;
+    pinocchio::GeometryData collision_data;
+    Eigen::VectorXd q;
+};
+
+
+
+class DemoPanel : public rviz_common::Panel
+{
+  Q_OBJECT
+public:
+  explicit DemoPanel(QWidget * parent = 0);
+  ~DemoPanel() override;
+
+  void onInitialize() override;
+
+protected:
+  std::shared_ptr<rviz_common::ros_integration::RosNodeAbstractionIface> node_ptr_;
+  //rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+
+  void topicCallback(const std_msgs::msg::String& msg); // callback for topic robot_description
+
+  QLabel * label_titleframe_transform_; // label to show possible collisions
+  QLabel * label_frame_transform_; // label to show possible collisions
+  QLabel * label_title_collision_; // label to show possible collisions
+  QLabel * label_collision_; // label to show possible collisions
+  QPushButton * button_; // button to calculate collision and other stuff
+  QComboBox * dropdown_; // dropdown menu to select options
+  
+
+  std::string jacobian_; // string to store jacobian
+  std::string collision_pairs_; // string to store collision pairs
+
+  // obj for PinocchioManager
+  PinocchioManager pinocchio_manager_obj_;
+
+
+private Q_SLOTS:
+  void buttonActivated();
+  
+};
+
+
+
+
+
+
+
+}  // namespace rviz_panel_pinocchio_tiago
+
+#endif  // RVIZ_PANEL_PINOCCHIO_TIAGO__DEMO_PANEL_HPP_
---
+++ src/pinocchio_manage.cpp
@@ -0,0 +1,93 @@
+// Description: Class to manage the Pinocchio model
+
+#include <rviz_panel_pinocchio_tiago/demo_panel.hpp>
+
+#define PINOCCHIO_ENABLE_COMPATIBILITY_WITH_VERSION_2
+
+
+namespace rviz_panel_pinocchio_tiago
+{
+// Class to manage the Pinocchio model 
+PinocchioManager::PinocchioManager(const std::string& urdf_xml, const std::string& package_share_directory) {
+    
+    //build the model from the URDF
+    pinocchio::urdf::buildModelFromXML(urdf_xml, model);
+    pinocchio::urdf::buildGeom(model, std::istringstream(urdf_xml), pinocchio::VISUAL, visual_model);
+    pinocchio::urdf::buildGeom(model, std::istringstream(urdf_xml), pinocchio::COLLISION, collision_model);
+
+    // add all collision pairs from srdf file
+    collision_model.addAllCollisionPairs();
+    pinocchio::srdf::removeCollisionPairs(model, collision_model, package_share_directory + "/srdf/tiago.srdf", false);
+
+    // create data model
+    pinocchio::Data data_model(model);
+    data = data_model;
+    
+    // create collision data model
+    pinocchio::GeometryData collision_data_model(collision_model);
+    collision_data = collision_data_model;
+}
+
+void PinocchioManager::setConfiguration() {
+    std::cout << "Joint configuration: " << std::endl << model.nq << std::endl << std::endl;
+    Eigen::VectorXd config(model.nq);
+    q = config;
+    //TODO set the configuration with custom values
+    q << 0.0, 0.0, 0.0, 1.36, -0.0, -0.0, 2.22, 0.0, -0.0, -0.0, -0.0, -0.0, -0.0, -0.0, 0.0, 0.0;
+    std::cout << "Configuration done " << std::endl;
+}
+
+
+std::vector<std::string>  PinocchioManager::getConfiguration() {
+    std::vector<std::string> frame_names;
+    for (int joint_id = 0; joint_id < model.njoints; ++joint_id) {
+        frame_names.push_back(model.names[joint_id]);
+    }
+
+    return frame_names; 
+}
+
+
+void PinocchioManager::performForwardKinematics() {
+    pinocchio::framesForwardKinematics(model, data, q);
+}
+
+
+std::string PinocchioManager::getFrameTransform(const std::string& arm_link_name) {
+    const auto arm_link = model.getFrameId(arm_link_name);
+    std::cout << "Frame transform: " << std::endl << data.oMf[arm_link] << std::endl;
+    std::ostringstream oss;
+    oss << data.oMf[arm_link];
+    return oss.str();
+}
+
+
+void PinocchioManager::performJacobian(const std::string& arm_link) {
+    Eigen::MatrixXd ee_jacobian(6, model.nv);
+    pinocchio::computeFrameJacobian(model, data, q, model.getFrameId(arm_link), ee_jacobian);
+}
+
+
+std::vector<std::string> PinocchioManager::performCollisionCheck() {
+    pinocchio::computeCollisions(model, data, collision_model, collision_data, q);
+    
+    std::vector<std::string> collision_pairs;
+
+    for (size_t k = 0; k < collision_model.collisionPairs.size(); ++k)
+    {
+      const auto& cp = collision_model.collisionPairs[k];
+      const auto& cr = collision_data.collisionResults[k];
+      if (cr.isCollision())
+      {
+        const auto& body1 = collision_model.geometryObjects[cp.first].name;
+        const auto& body2 = collision_model.geometryObjects[cp.second].name;
+        
+        collision_pairs.push_back("Collision detected between " + body1 + " and " + body2);
+        std::cout << "Collision detected between " << body1 << " and " << body2 << std::endl;
+      }
+    }
+
+    return collision_pairs;
+}
+
+} // namespace rviz_panel_pinocchio_tiago
